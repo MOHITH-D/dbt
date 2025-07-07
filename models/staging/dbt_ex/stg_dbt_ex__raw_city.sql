4cdcbc4774ec56acc2231faf43cebe55141c3461
Mohith D <dmohith951@gmail.com>
2025-07-07 09:12:40 +0000

    nothing

--- models/staging/dbt_ex/stg_dbt_ex__raw_city.sql
+++ models/staging/dbt_ex/stg_dbt_ex__raw_city.sql
@@ -4,14 +4,6 @@ source as (
 
     select * from {{ source('dbt_ex', 'raw_city') }}
 
-),
-
-renamed as (
-
-    select
-
-    from source
-
 )
 
-select * from renamed
+select * from source
--- models/staging/dbt_ex/stg_dbt_ex__raw_item.sql
+++ models/staging/dbt_ex/stg_dbt_ex__raw_item.sql
@@ -4,14 +4,6 @@ source as (
 
     select * from {{ source('dbt_ex', 'raw_item') }}
 
-),
-
-renamed as (
-
-    select
-
-    from source
-
 )
 
-select * from renamed
+select * from source
