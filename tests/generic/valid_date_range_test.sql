a33b79159683b73e17c025ffa4d2b675bf4259ee
Mohith D <dmohith951@gmail.com>
2025-07-08 10:20:34 +0000

    valid_date test

--- tests/generic/valid_date_range_test.sql
+++ tests/generic/valid_date_range_test.sql
@@ -1,12 +1,11 @@
+
 {% test valid_date(model, column_name) %}
-    with
-        invalid_dates as (
-            select *
-            from {{ ref(model) }}
-            where
-                {{ column_name }} < '2025-07-06' or {{ column_name }}  > current_date
-        )
-    select count(*) as invalid_date_count
+    with invalid_dates as (
+        select *
+        from {{ ref(model) }}
+        where {{ column_name }} > current_date
+    )
+
+    select *
     from invalid_dates
-    having count(*) > 0;
 {% endtest %}
