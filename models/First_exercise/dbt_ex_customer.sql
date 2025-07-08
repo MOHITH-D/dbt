2108abe49f385af2de7ea328951bab4c2d844ca3
Mohith D <dmohith951@gmail.com>
2025-07-08 11:56:15 +0000

    dbt_ex_customer

--- models/First_exercise/dbt_ex_customer.sql
+++ models/First_exercise/dbt_ex_customer.sql
@@ -15,20 +15,3 @@ select * from source_data
 {% if is_incremental() %}
  where c_custkey > (select max(c_custkey) from {{ this }}) 
 {% endif %}
-
--- changed_records as (
---     select src.* from source_data src
---     left join {{this}} tgt on src.c_custkey=tgt.c_custkey
---     where 
---         tgt.c_custkey is NULL
---         or (
---             src.c_name <> tgt.c_name or
---             src.c_address <> tgt.c_address OR
---             src.c_nationkey <> tgt.c_nationkey or
---             src.c_phone <> tgt.c_phone or
---             src.c_acctbal <> tgt.c_acctbal or
---             src.c_mktsegment <> tgt.c_mktsegment or
---             src.c_comment <> tgt.c_comment
---         )
--- )
--- select * from changed_records
