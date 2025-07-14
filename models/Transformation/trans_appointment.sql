91dcbd689e7e40ea63e17bfe7e7e50192b9a29dc
Mohith D <dmohith951@gmail.com>
2025-07-14 06:16:40 +0000

    staging made in snowflake and dbt

---
+++ models/Transformation/trans_appointment.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+
+select 
+    appointment_id as appointment_key,
+    patient_id as patient_key,
+    doctor_id as doctor_key,
+    appointment_date,
+    appointment_time,
+    reason_for_visit,
+    status
+from {{ ref('stg_raw__appointments') }}
--- models/staging/raw/stg_raw__appointments.sql
+++ models/staging/raw/stg_raw__appointments.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        schema='staging'
+    )
+}}
+
+
 with 
 
 source as (
