bfd2c833eb26860f3ce532d27c0d9a21825c40f3
Mohith D <dmohith951@gmail.com>
2025-07-15 07:31:53 +0000

    fact_appointment done

---
+++ models/marts/fact_appointment.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        schema='marts'
+    )
+}}
+select 
+    a.appointment_key,
+    p.patient_id as patient_key,
+    d.doctor_id as doctor_key,
+    a.appointment_date,
+    a.appointment_time,
+    a.reason_for_visit,
+    a.status
+from {{ ref('trans_appointment') }} a 
+LEFT JOIN {{ ref('trans_patient') }} p on a.patient_key=p.patient_id
+LEFT JOIN {{ ref('trans_doctor') }} d on a.doctor_key=d.doctor_id
