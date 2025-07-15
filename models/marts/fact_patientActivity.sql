137035e33e005a9954734fd351345c191dd9860f
Mohith D <dmohith951@gmail.com>
2025-07-15 09:04:15 +0000

    done

--- models/marts/fact_patientActivity.sql
+++ models/marts/fact_patientActivity.sql
@@ -7,10 +7,10 @@
 with appointments as (
     select
         patient_key,
-        COUNT(*) as total_appointments,
-        COUNT(case when status = 'No-show' then 1 end) as no_show_count,
-        MIN(appointment_date) as first_appointment_date,
-        MAX(appointment_date) as last_appointment_date
+        count(*) as total_appointments,
+        count(case when status = 'No-show' then 1 end) as no_show_count,
+        min(appointment_date) as first_appointment_date,
+        max(appointment_date) as last_appointment_date
     from {{ ref('fact_appointment') }}
     group by patient_key
     order by patient_key asc
@@ -19,18 +19,18 @@ with appointments as (
 billing as (
     select
         patient_key,
-        SUM(amount) as total_billed,
-        SUM(case when payment_status = 'Paid' then amount else 0 end) as total_paid
+        sum(amount) as total_billed,
+        sum(case when payment_status = 'Paid' then amount else 0 end) as total_paid
     from {{ ref('fact_billing') }}
     group by patient_key
 )
 
 select
-    COALESCE(a.patient_key, b.patient_key) as patient_key,
-    COALESCE(total_appointments, 0) as total_appointments,
-    COALESCE(total_billed, 0) as total_billed,
-    COALESCE(total_paid, 0) as total_paid,
-    COALESCE(no_show_count, 0) as no_show_count,
+    coalesce(a.patient_key, b.patient_key) as patient_key,
+    coalesce(total_appointments, 0) as total_appointments,
+    coalesce(total_billed, 0) as total_billed,
+    coalesce(total_paid, 0) as total_paid,
+    coalesce(no_show_count, 0) as no_show_count,
     a.first_appointment_date,
     a.last_appointment_date
 from appointments a
